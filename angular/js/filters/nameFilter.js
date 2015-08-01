1da5e2408dcc4b949d893d007823862f26130e6b
jefter <jeftarmascarenhas@hotmail.com>
2015-08-01 02:16:03 -0300

    Create Filter Name

--- angular/js/filters/nameFilter.js
+++ angular/js/filters/nameFilter.js
@@ -5,12 +5,12 @@
 angular.module('filtersBotan',[]).filter('name',function () {
 	return function (input) {
 		var listaDeNomes = input.split(' ');
-		if(/(da|de)/.test(name)) return name;
 		var listaDeNomesFormatada = listaDeNomes.map(function (name) {
+			if(/(da|de)/.test(name)) return name;
 			return name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
 		});
 		console.log(listaDeNomesFormatada);
-		return listaDeNomesFormatada.join(' ');
+		return listaDeNomesFormatada.join(" ");
 	};
 });
 
