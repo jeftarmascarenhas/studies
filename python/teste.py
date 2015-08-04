fc348bc88aa87ee99be9fc7f62e155856f7754d6
jefter <jeftarmascarenhas@hotmail.com>
2015-08-03 22:17:05 -0300

    python general update

--- python/teste.py
+++ python/teste.py
@@ -1,1 +1,15 @@
-print('Hello World');
+print('Hello World')
+
+#variable
+a = 10.5 #Float
+b = 25 #int
+print(a+b)
+
+texto = 'Todo texto é uma string' #String
+print(type(a)) #Show type variable
+
+#Concatenation
+result = (a*b) / 2
+print("O valor e: ",result)
+print("O valor e: %i" %result)
+print("O valor e: " + str(result))
