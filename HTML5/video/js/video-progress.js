ba74cc58fa86dd553b3370c9f8e3ef052b3eb201
jefter <jeftarmascarenhas@hotmail.com>
2015-08-21 01:32:39 -0300

    add interaction bar progress and volume +

--- HTML5/video/js/video-progress.js
+++ HTML5/video/js/video-progress.js
@@ -14,10 +14,15 @@ function init() {
 	progress = document.querySelector('.progress');
 	bar = document.querySelector('.bar');
 	playPause = document.querySelector('.play-pause');
+	minVolume = document.querySelector('.minVolume');
 
 	playPause.addEventListener('click', playPauseAction, false);
 
-	// progress.addEventListener('click', progressInteractions, false);
+	bar.addEventListener('click', progressInteractions, false);
+
+	minVolume.addEventListener('click', volumeMin, false);
+
+
 	
 };
 
@@ -44,6 +49,21 @@ function states () {
 		var total = parseInt(myvideo.currentTime*maximo/myvideo.duration);
 		progress.style.width = total+'px';
 	}
+};
+
+function progressInteractions (position) {
+	if((myvideo.paused == false) && (myvideo.ended == false)){
+		var ratonX = position.pageX - bar.offsetLeft;
+		var newTime = ratonX * myvideo.duration /maximo;
+
+		myvideo.currentTime = newTime;
+
+		progress.style.width = ratonX+'px';
+	}
+};
+
+function volumeMin () {
+	myvideo.volume-=0.1;
 }
 
 window.addEventListener('load', init, false);
