7144553c5f7197488429bf94c1287393c2aad40b
jefter <jeftarmascarenhas@hotmail.com>
2015-09-27 01:38:20 -0300

    Studies development with NodeJS and ExpressJS

---
+++ javascript/nodejs/express/meandowlark.js
@@ -0,0 +1,50 @@
+var express = require('express')
+  , app = express()
+  ;
+
+app.set('port', process.env.PORT || 3000);
+
+var fortunes =[
+"Conquer your fears or they will conquer you.",
+ "Rivers need springs.",
+ "Do not fear what you don't know.",
+ "You will have a pleasant surprise.",
+ "Whenever possible, keep it simple."
+
+];
+
+var handlebars = require('express3-handlebars')
+ .create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(__dirname + '/public'));
+
+  app.get('/', function (req, res) {
+      res.render('home');
+  });
+
+  app.get('/about', function (req, res) {
+      var randomFortune =
+            fortunes[Math.floor(Math.random() * fortunes.length)];
+      res.render('about', {fortune: randomFortune});
+  })
+
+  //custom 404
+  app.use(function (req, res) {
+      res.status(404);
+      res.render('404');
+  });
+
+  //custom 500
+  app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render(500);
+  })
+
+  app.listen(app.get('port'), function () {
+    console.log('Express started on http//:localhost:3000' +
+    app.get('port') + '; press Ctrl+C to terminate.');
+  });
